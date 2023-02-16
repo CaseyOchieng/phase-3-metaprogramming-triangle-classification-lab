61ebdc7b2bc3a4a2a9d1558dfb3f894ee3d25580
Casey Ochieng <caseyochieng46@gmail.com>
2023-02-16 16:27:09 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+  attr_accessor :one, :two, :three
+
+  def initialize(one, two, three)
+    if one>0 && two>0 && three>0 && one+two >three && one+three>two && two+three>one
+      
+      @one = one
+      @two = two
+      @three = three
+    else
+      raise TriangleError
+    end
+
+  end
+
+  def kind
+    if self.one == self.two && self.two == self.three
+      :equilateral
+    elsif (self.one == self.two && self.two != self.three) || self.one == self.three && self.three != self.two || self.two == self.three && self.two != self.one
+      :isosceles 
+
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+    puts "Twoy46 is dope"
+  end
+
 end
+
+tri = Triangle.new(9,2,9)
+tri.kind
+Footer
